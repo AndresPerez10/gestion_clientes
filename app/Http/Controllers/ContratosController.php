5662f9e0b3faca1b6d3ec34df180b94670bae77e
LAPTOP-KFVBKRCQ\PTVTELECOM <andresperezreyes50@gmail.com>
2025-03-03 10:33:40 +0100

    endpoint implementado

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -11,32 +11,7 @@ use Illuminate\Support\Facades\Validator;
 class ContratosController extends Controller
 {
 
-    public function obtenerContratosPorCliente(Request $datos) {
-
-        $validatedData = ['dni' => $datos->route('dni')];
-
-        $validator = Validator::make($validatedData, 
-            ['dni' => 'regex:/^(\d{8})([A-Z])$/'],
-            ['dni.regex' => 'El formato de dni no es correcto']);
-
-        if ($validator->fails()) {
-            return $this->devolverRespuestas( false, '422', $validator->errors());
-        }
-
-        try {
-            $cliente = Cliente::where('dni', $validatedData['dni'])->first();
-            if(is_null($cliente)) {
-                Log::error('ocurrió un error: No se encuentra el dni ');
-                return $this->devolverRespuestas( true, '404', 'Dni inexistente.');
-            }
-            $contratos = $cliente->contratos;
-            // $contratos[] = $cliente->contratosCustom($cliente->dni);
-            return $this->devolverRespuestas( true, '200', $contratos);
-        } catch(\Exception $e) {
-            Log::error('ocurrió un error: Fallo en el servidor');
-            return $this->devolverRespuestas( false, '500', $e);
-        }         
-    }
+    
     
 
     private function devolverRespuestas($status, $httpCode, $message) {
