cfb835b6d8a7b625e0c1d9bdd564d835548a7267
LAPTOP-KFVBKRCQ\PTVTELECOM <andresperezreyes50@gmail.com>
2025-03-03 10:11:53 +0100

    actualizar contratos

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientesController; 
+use App\Http\Controllers\ContratosController;
 
+//Clientes
 Route::get('/clientes/obtener', [ClientesController::class, 'obtenerClientes']);
 Route::post('/clientes/crear', [ClientesController::class, 'crearCliente']);
 Route::get('/clientes/obtener/{dni}', [ClientesController::class, 'obtenerClientePorDni']);
 Route::delete('/clientes/eliminar/{dni}', [ClientesController::class, 'eliminarClientePorDni']);
 Route::put('/clientes/actualizar/{dni}', [ClientesController::class, 'actualizarCliente']);
+
+//Contratos
+
+Route::get('/contratos/obtener', [ContratosController::class, 'obtenerContratos']);
+Route::get('/contratos/obtener/{dni}', [ContratosController::class, 'obtenerContratosPorCliente']);
+Route::post('/contratos/crear', [ContratosController::class, 'crearContrato']);
+Route::delete('/contratos/eliminar', [ContratosController::class, 'eliminarContratoPorDni']);
